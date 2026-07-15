f231c9f72fb18d098b62ebcc6564c37504185cb9
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-07-15 19:39:12 +0300

    fix scroll - content shift

--- src/components/layout/PagesWrapper/PagesWrapper.jsx
+++ src/components/layout/PagesWrapper/PagesWrapper.jsx
@@ -25,31 +25,23 @@ import { useTransitionState } from "next-transition-router";
 
 // #endregion ===========================
 
-const SCROLLBAR_WIDTH_PX = 15;
-
 export default function PagesWrapper({ children }) {
 	const { stage, isReady } = useTransitionState();
 	const ref = React.useRef(null);
 
-  // 1. block scroll during transition
-  // 2. preserve space (avoid content movement)
+	// block scroll during transition
 	React.useEffect(() => {
 		if (!ref.current || !window || !document) return;
 
 		let appliedOverflow;
-		let appliedWidth;
+
 		if (stage === "none" && isReady) {
 			appliedOverflow = "visible";
-			appliedWidth = "100%";
-			document.documentElement.classList.remove("custom_scroll");
 		} else {
 			appliedOverflow = "hidden";
-			appliedWidth = `${window.innerWidth - SCROLLBAR_WIDTH_PX}px`;
-			document.documentElement.classList.add("custom_scroll");
 		}
 
 		document.body.style.overflow = appliedOverflow;
-		ref.current.style.width = appliedWidth;
 	}, [isReady, stage]);
 
 	return (
